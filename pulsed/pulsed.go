affaaa2ef0d1127b3ad50452d62220c093a10e5f
Danny van Heumen <danny@dannyvanheumen.nl>
2014-05-31 18:40:05 +0200

    Made pulsed configurable + no global var anymore.

--- pulsed/pulsed.go
+++ pulsed/pulsed.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"github.com/cobratbq/pulse"
 	"github.com/gorilla/mux"
@@ -8,72 +9,89 @@ import (
 	"net/http"
 )
 
-var p *pulse.Connection
+type configuration struct {
+	DatabaseHost string
+	DatabaseName string
+	Collection   string
+	Port         uint
+}
 
 func main() {
+	// initialize with specified program arguments
+	var config configuration
+	configure(&config)
+
 	// initialize connection to mongodb
-	var connectString = "mongodb://localhost/pulse"
-	conn, err := pulse.Dial(connectString, "pulse", "pulses")
+	var connectString = fmt.Sprintf("mongodb://%s", config.DatabaseHost)
+	conn, err := pulse.Dial(connectString, config.DatabaseName, config.Collection)
 	if err != nil {
 		log.Printf("Failed to connect to pulse database '%s': %s.", connectString, err.Error())
 		return
 	}
 	defer conn.Close()
-	p = conn
 	log.Println("Connection to mongoDB established.")
 
 	// initialize http pulse server
 	r := mux.NewRouter()
-	r.HandleFunc("/", home)
-	r.HandleFunc("/pulse", pulseInfo)
-	r.HandleFunc("/show/{namespace}", pulseShow)
-	r.HandleFunc("/pulse/{namespace}", pulseRecord)
+	r.HandleFunc("/", info)
+	r.HandleFunc("/show/{namespace}", createPulseShowHandler(conn))
+	r.HandleFunc("/pulse/{namespace}", createPulseRecordHandler(conn))
 	http.Handle("/", r)
 
-	log.Println("Starting http server ...")
-	log.Println(http.ListenAndServe(":8080", nil))
+	// start http pulse server
+	log.Printf("Starting http server on :%d ...", config.Port)
+	log.Fatalln(http.ListenAndServe(fmt.Sprintf(":%d", config.Port), nil))
 }
 
-func home(resp http.ResponseWriter, req *http.Request) {
-	defer req.Body.Close()
-	resp.Write([]byte("Hello world!"))
+func configure(config *configuration) {
+	flag.UintVar(&config.Port, "port", 8000, "Pulse server port.")
+	flag.StringVar(&config.DatabaseHost, "dbhost", "localhost", "Database host.")
+	flag.StringVar(&config.DatabaseName, "dbname", "pulse", "Database name.")
+	flag.StringVar(&config.Collection, "collection", "pulses", "Name of collection for storing pulses.")
+	flag.Parse()
 }
 
-func pulseInfo(resp http.ResponseWriter, req *http.Request) {
+func info(resp http.ResponseWriter, req *http.Request) {
 	defer req.Body.Close()
-	resp.Write([]byte("Pulse to your own namespace, e.g. '/pulse/my-namespace'"))
+	resp.Write([]byte("Usage:\n"))
+	resp.Write([]byte("* Record a pulse to your own namespace, e.g. '/pulse/test' for namespace 'test'.\n"))
+	resp.Write([]byte("* Show previously recorded pules for your own namespace, e.g. '/show/test' for namespace 'test'.\n"))
 }
 
-func pulseShow(resp http.ResponseWriter, req *http.Request) {
-	defer req.Body.Close()
-	vars := mux.Vars(req)
-	ns, ok := vars["namespace"]
-	if !ok {
-		log.Println("Failed to find namespace path variable.")
-		resp.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-	pulses, err := p.Get(ns)
-	if err != nil {
-		log.Println(err.Error())
-		resp.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-	for _, pulse := range pulses {
-		fmt.Fprintf(resp, "%s,%s\n", pulse.Namespace, pulse.Time.String())
+func createPulseShowHandler(conn *pulse.Connection) func(http.ResponseWriter, *http.Request) {
+	return func(resp http.ResponseWriter, req *http.Request) {
+		defer req.Body.Close()
+		vars := mux.Vars(req)
+		ns, ok := vars["namespace"]
+		if !ok {
+			log.Println("Failed to find namespace path variable.")
+			resp.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		pulses, err := conn.Get(ns)
+		if err != nil {
+			log.Println(err.Error())
+			resp.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		for _, pulse := range pulses {
+			fmt.Fprintf(resp, "%s,%s\n", pulse.Namespace, pulse.Time.String())
+		}
 	}
 }
 
-func pulseRecord(resp http.ResponseWriter, req *http.Request) {
-	defer req.Body.Close()
-	vars := mux.Vars(req)
-	ns, ok := vars["namespace"]
-	if !ok {
-		log.Println("Failed to find namespace path variable. Not recording this pulse.")
-		resp.WriteHeader(http.StatusInternalServerError)
-		return
+func createPulseRecordHandler(conn *pulse.Connection) func(http.ResponseWriter, *http.Request) {
+	return func(resp http.ResponseWriter, req *http.Request) {
+		defer req.Body.Close()
+		vars := mux.Vars(req)
+		ns, ok := vars["namespace"]
+		if !ok {
+			log.Println("Failed to find namespace path variable. Not recording this pulse.")
+			resp.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		log.Printf("PULSE '%s'", ns)
+		conn.Record(ns)
+		resp.Write([]byte("OK"))
 	}
-	log.Printf("PULSE '%s'", ns)
-	p.Record(ns)
-	resp.Write([]byte("OK"))
 }
